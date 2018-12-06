95c79a55c01d360d49fe8ef8fc71e527b00a259b
Stian Lode <stian.lode@canrig.com>
2018-12-06 16:51:09 +0100

    Persistence and api soon done.

--- lodex.py
+++ lodex.py
@@ -1,6 +1,13 @@
-import copy
+#!/usr/bin/env python3
 
-class MemLog:
+import argparse
+import pickle
+import sys
+
+from enum import Enum
+Type = Enum("Type", "LEAF NODE MEM_NODE ROOT_NODE")
+
+class DirtyBlocks:
     def __init__(self):
         self.log = []
 
@@ -17,29 +24,48 @@ class MemLog:
     def __len__(self):
         return len(self.log)
 
-class Log:
-    def __init__(self):
-        self.log = []
+class FileLog:
+    def __init__(self, filename):
+        self.filename = filename
+        self.file = open(filename, "wb+")
+
+        if len(self) == 0:
+            print("Create database")
+            self.write_checkpoint(0)
+            self.write_checkpoint(self.put(IndexBlock()))
+        else:
+            print("open database")
+
+    def write_checkpoint(self, offset):
+        self.file.seek(0, 0)
+        self.file.write(offset.to_bytes(4, byteorder="big"))
+        self.file.write(offset.to_bytes(4, byteorder="big"))
+        self.file.flush()
+
+    def read_checkpoint(self):
+        self.file.seek(0, 0)
+        offset = int.from_bytes(self.file.read(4), byteorder="big")
+        offset2 = int.from_bytes(self.file.read(4), byteorder="big")
+        assert(offset == offset2)
+        return offset
 
     def put(self, value):
-        self.log.append(value)
-        return len(self.log) - 1
-
+        offset = len(self)
+        value_bytes = pickle.dumps(value)
+        length = len(value_bytes)
+        self.file.write(length.to_bytes(4, byteorder="big"))
+        self.file.write(value_bytes)
+        return offset
+    
     def get(self, offset):
-        return copy.deepcopy(self.log[offset])
-
-    def get_last(self):
-        return copy.deepcopy(self.log[-1])
+        self.file.seek(offset, 0)
+        length = int.from_bytes(self.file.read(4), byteorder="big")
+        value_bytes = self.file.read(length)
+        return pickle.loads(value_bytes)
 
     def __len__(self):
-        return len(self.log)
-
-from enum import Enum
-Type = Enum("Type", "LEAF NODE MEM_NODE ROOT_NODE")
-
-class CheckPointBlock:
-    def __init__(self):
-        self.root_offset = 0
+        self.file.seek(0, 2)
+        return self.file.tell()
 
 class IndexBlock:
     def __init__(self):
@@ -48,8 +74,6 @@ class IndexBlock:
     def has(self, key_frag):
         return key_frag in self.index_block
 
-    # type NODE, LEAF, cache
-    # Use sorted list for c impl
     def put(self, key_frag, key, value, typ):
         self.index_block[key_frag] = (key, value, typ)
 
@@ -59,22 +83,22 @@ class IndexBlock:
     def keys(self):
         return sorted(self.index_block.keys())
 
-def split_by_n( seq, n ):
+def split_by_n(seq, n):
     while seq:
         yield seq[:n]
         seq = seq[n:]
 
-class TransactionalIndex:
-    def __init__(self, log):
+class LogIndex:
+    def __init__(self, log, root_offset):
         self.log = log
-        self.root = log.get_last()
-        self.in_memory_blocks = MemLog()
+        self.root = log.get(root_offset)
+        self.in_memory_blocks = DirtyBlocks()
 
     def walk(self, callback, internal_nodes=False):
         def rec_do(block, depth):
             for subkey in block.keys():
                 entry = block.get(subkey)
-                print("    " * depth + subkey + ": " + str(entry))
+                #print("    " * depth + subkey + ": " + str(entry))
                 if entry[2] == Type.NODE:
                     rec_do(self.log.get(entry[1]), depth + 1)
                     if internal_nodes:
@@ -97,38 +121,36 @@ class TransactionalIndex:
             if block.has(subkey) == False:
                 block.put(subkey, key, value, Type.LEAF)
                 return
-            elif block.get(subkey)[2] == Type.NODE: # is NODE. bring into CACHE
+            elif block.get(subkey)[2] == Type.NODE:
                 child_block = self.log.get(block.get(subkey)[1])
                 child_block_index = self.in_memory_blocks.put(child_block)
                 block.put(subkey, None, child_block_index, Type.MEM_NODE)
                 block = child_block
                 continue
-            elif block.get(subkey)[2] == Type.MEM_NODE: # is cached NODE. 
+            elif block.get(subkey)[2] == Type.MEM_NODE:
                 child_block = self.in_memory_blocks.get(block.get(subkey)[1])
                 block = child_block
                 continue
             elif block.get(subkey)[2] == Type.LEAF:
-                old_entry = block.get(subkey) # key-value
+                old_entry = block.get(subkey)
                 if old_entry[0] == key:
                     block.put(subkey, key, value, Type.LEAF)
                     return
                 else:
-                    new_block = IndexBlock() # block
+                    new_block = IndexBlock()
                     index = self.in_memory_blocks.put(new_block)
                     block.put(subkey, None, index, Type.MEM_NODE)
                     self.put(old_entry[0], old_entry[1])
                     self.put(key, value)
                     return
             else:
-                raise ValueError("Illegal element type.")
+                raise ValueError("Should not happen. Illegal element type.")
 
-        # We don't have any subkeys left, hence, the block
-        # we're looking at is also a leaf. We add the empty value
+        # NODE is also LEAF
         block.put("", key, value, Type.LEAF)
 
     def get(self, key):
         block = self.root
-        entry = None
         for subkey in split_by_n(key, 2):
             if block.has(subkey):
                 entry = block.get(subkey)
@@ -136,8 +158,8 @@ class TransactionalIndex:
                 if entry_type == Type.LEAF: # LEAF
                     if entry[0] == key:
                         return entry[1]
-                    else:
-                        raise KeyError("Could not find key {} in block {}".format(key, entry))
+                    raise KeyError(
+                        "Could not find key {} in block {}".format(key, entry))
                 elif entry_type == Type.NODE: # directory
                     block_offset = block.get(subkey)
                     block = self.log.get(block_offset[1])
@@ -153,6 +175,7 @@ class TransactionalIndex:
             entry = block.get("")
             if entry[0] == key:
                 return entry[1]
+        raise KeyError(key + " not found")
 
     def commit(self):
         if not len(self.in_memory_blocks):
@@ -166,20 +189,29 @@ class TransactionalIndex:
                     block.put(subkey, entry[0], block_id, Type.NODE)
             return self.log.put(block)
 
-        root_index = commit_rec(self.root)
-        self.root = log.get(root_index)
-        self.in_memory_blocks = MemLog()
-        return root_index
+        root_offset = commit_rec(self.root)
+        self.log.write_checkpoint(root_offset)
+        self.root = log.get(root_offset)
+        self.in_memory_blocks = DirtyBlocks()
+        assert self.log.read_checkpoint() == root_offset
+        print(root_offset)
+        return root_offset
 
-if __name__ == "__main__":
-    log = Log()
-    log.put(CheckPointBlock())
-    log.put(CheckPointBlock())
-    log.put(IndexBlock())
-    index = TransactionalIndex(log)
+if True:
+    log = FileLog("database_test.ldx")
+    print(log.read_checkpoint())
+    index = LogIndex(log, log.read_checkpoint())
+
+    counter = 0
+    def tmp(key, value):
+        global counter
+        counter += 1
+        print("{}: {}".format(key, log.get(value)))
+
+    index.walk(tmp)
 
     import uuid
-    for i in range(1000):
+    for i in range(10):
         key = uuid.uuid4().hex[:20]
         idx = log.put(key)
         index.put(key, idx)
@@ -191,20 +223,94 @@ if __name__ == "__main__":
         index.put(key, idx)
         assert(index.get(key) == idx)
 
-
     counter = 0
-    import sys
     def tmp(key, value):
         global counter
         counter += 1
+        print("{}: {}".format(key, log.get(value)))
 
-    index.walk(tmp, internal_nodes=True)
     index.commit()
-    print("COMMIT")
-    index.walk(tmp, internal_nodes=True)
-    index.put("testa", log.put("testa"))
-    print("ADDED test:test")
-    index.walk(tmp, internal_nodes=True)
-    index.commit()
-    print("COMMIT")
-    index.walk(tmp, internal_nodes=True)
+    index2 = LogIndex(log, log.read_checkpoint())
+    index2.put("testa", log.put("testa_valuevalue"))
+    print(log.get(index2.get("testa")))
+    index2.commit()
+    index2.walk(tmp)
+    print(counter)
+    exit(1)
+
+
+if __name__ == "__main__":
+    main()
+
+def main():
+    main_parser = argparse.ArgumentParser(description='Lodex database API.')
+    main_parser.add_argument(
+            "--db",
+            type=str,
+            default="database.ldx",
+            help="Database filename.")
+    sub_parsers = main_parser.add_subparsers(dest='operation')
+    put_parser = sub_parsers.add_parser("put")
+    put_parser.add_argument("key")
+    put_parser.add_argument("value")
+    get_parser = sub_parsers.add_parser("get")
+    get_parser.add_argument("key")
+    del_parser = sub_parsers.add_parser("delete")
+    del_parser.add_argument("key")
+    status_parser = sub_parsers.add_parser("status")
+    dump_parser = sub_parsers.add_parser("dump")
+    dump_parser.add_argument(
+            "--sep",
+            type=str,
+            default="\t",
+            help="key-value separator.")
+    load_parser = sub_parsers.add_parser("load")
+    load_parser.add_argument(
+            "--sep",
+            type=str,
+            default="\t",
+            help="key-value separator.")
+
+    args = main_parser.parse_args()
+
+    if args.db and args.operation:
+        log = FileLog(args.db)
+        index = LogIndex(log, log.read_checkpoint())
+
+        if args.operation == "put":
+            log_offset = log.put(args.value)
+            print(log_offset)
+            index.put(args.key, log.put(args.value))
+            index.commit()
+        elif args.operation == "get":
+            try:
+                print(log.get(index.get(args.key)))
+            except KeyError:
+                sys.stderr.write("Unable to find item '{}'".format(args.key))
+                exit(-1)
+        elif args.operation == "delete":
+            index.put(args.key, None)
+            index.commit()
+        elif args.operation == "status":
+            counter = 0
+            def item_counter(_1, _2):
+                counter += 1
+            index.walk(item_counter)
+            print("Stats for {}".format(log.filename))
+            print(" items: {}".format(counter))
+            print(" size: {}".format(len(log)))
+        elif args.operation == "export":
+            def item_printer(key, value):
+                print("{}{}{}".format(key, args.sep, value))
+            index.walk(item_printer)
+        elif args.operation == "import":
+            try:
+                for line in sys.stdin.readline():
+                    key, value = line.split(args.sep)
+                    index.put(key, log.put(value))
+                index.commit()
+            except:
+                sys.stderr.write("Failed to load values.")
+                exit(-1)
+        exit(0)
+    exit(-2)
